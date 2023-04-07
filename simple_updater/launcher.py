fddb38805b0eda55633b23b6055326e45f6c417b
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2023-04-06 23:08:09 -0400

    Hashchecker done

--- simple_updater/launcher.py
+++ simple_updater/launcher.py
@@ -1,5 +1,5 @@
 # Simple BoomerangNet Cyclon launcher program
-import os, sys, json, requests
+import os, sys, json, requests, hashlib
 
 if not os.path.exists('./config.json'):
     print("Unable to load the config file!")
@@ -53,5 +53,27 @@ class CyclonLauncher:
         uri = self.network.get('hashUri')
         version = self.version.replace('.', '_')
         hashList = self.getNetworkData(f'{uri}hashList_{version}.json')
+        if hashList.get("systemVersion", "") != self.version:
+            print('Version mismatch!')
+            sys.exit()
+
+        head_folder = self.common.get('headFolder', '')
+
+        for file in hashList.get('hashes', []):
+            filepath = head_folder + file.get('name', '')
+            if not os.path.exists(filepath):
+                print(f'{filepath} does not exist!')
+                print('Please check your install!')
+                sys.exit()
+
+            if not self.launcher.get('skipHashCheck', False):
+                with open(filepath, 'rb') as checkFile:
+                    hash_ = hashlib.md5(checkFile.read())
+
+                if file.get('hash', '') != hash_.hexdigest():
+                    print(f'{filepath} has a hash mismatch!')
+                    if self.launcher.get('haltOnMismatch', True):
+                        sys.exit()
+
 
 CyclonLauncher()
