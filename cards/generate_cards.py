41020dbc34106e7a3eaaede70314634b97b6b2bc
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2024-03-04 12:37:12 -0500

    Add card generation tool

---
+++ cards/generate_cards.py
@@ -0,0 +1,39 @@
+import csv
+import random
+import string
+import binascii
+
+count = 10
+prefix = "BOOM"
+sector_key = [0x37, 0x21, 0x53, 0x6a, 0x72, 0x40]
+cardList = []
+
+def generate_random_hex(length):
+    hex_chars = string.hexdigits[:-6].upper()  # Exclude lowercase letters
+    return ''.join(random.choice(hex_chars) for _ in range(length))
+
+for i in range(count):
+    cardID = prefix + generate_random_hex(16)
+    while cardID in cardList:
+        print('Repeat ID!')
+        cardID = prefix + generate_random_hex(16)
+    cardList.append(cardID)
+
+with open('./cards.csv', 'w', newline='') as csvFile:
+    output = csv.writer(csvFile)
+    fields = ['PRINT', 'KEY', 'SECTOR0_BLOCK0', 'SECTOR0_BLOCK1']
+    output.writerow(fields)
+
+    for cardID in cardList:
+        print(cardID)
+        printCode = '-'.join(cardID[i:i+4] for i in range(0, len(cardID), 4))
+        cardID = binascii.hexlify(cardID.encode()).decode()
+        block0 = cardID[:32]  # First 32 characters
+        block1 = cardID[32:]  # Last 32 characters
+        
+        output.writerow([
+            printCode,
+            ''.join(format(byte, '02X') for byte in sector_key),
+            block0,
+            block1,
+        ])
